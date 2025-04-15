8568d06760673eadc9675753d0b6ba1d134718aa
Harshit Suyal <160107580+Harshit-Suyal@users.noreply.github.com>
2025-04-15 08:46:20 +0530

    Add files via upload

---
+++ adj_list_graph.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class adj_list_graph{
+
+	private LinkedList<Integer>adj[];
+	public adj_list_graph(int v) {
+		adj = new LinkedList[v];
+		for(int i=0;i<v;i++){
+			adj[i] = new LinkedList<Integer>();
+		}
+	}
+	
+	public void addedge(int s,int d){
+		adj[s].add(d);
+		adj[d].add(s);
+	} 
+	    	
+	public static void main(String[] args){
+	     Scanner sc  = new Scanner(System.in);
+	     System.out.println("Enter the number of vertices and edges: ");
+	     int v = sc.nextInt();
+	     int e = sc.nextInt();
+	     p1 g = new p1(v);
+	     System.out.println("Enter the edges in (u v) format: ");
+	     for(int i=0;i<e;i++){
+	     	int s = sc.nextInt();
+	     	int d = sc.nextInt();
+	     	g.addedge(s,d);
+	     }
+	     g.printgraph();
+  	}
+  	
+  	public void printgraph(){
+  		for (int i = 0; i < adj.length; i++) {
+			System.out.print("Vertex " + i + ":");
+			for (Integer neighbor : adj[i]) {
+		        	System.out.print(" " + neighbor);
+		    	}
+		    System.out.println();
+        	}
+  	}
+  	
+}
+
